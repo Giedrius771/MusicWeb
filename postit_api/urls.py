3768fee7fcd0a7752bd775716a6f6272158b40f9
Giedrius771 <giedriuspranevicius@inbox.lt>
2023-07-21 11:20:18 +0300

    Music Website project

--- postit_api/urls.py
+++ postit_api/urls.py
@@ -1,9 +1,22 @@
-from django.urls import path
-from .views import BandListCreateView, AlbumListCreateView, SongListCreateView, AlbumReviewListCreateView, AlbumReviewLikeListCreateView, AlbumReviewDeleteView, SongDeleteView, LikeDeleteView, CommentList, AllCommentList
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from .views import (
+    BandListCreateView, BandDetailView, BandCommentListView, AlbumListCreateView, AlbumDetailView, AlbumCommentListView,
+    SongListCreateView, AlbumReviewListCreateView, AlbumReviewLikeListCreateView,
+    AlbumReviewDeleteView, SongDeleteView, LikeDeleteView, CommentList, AllCommentList, SongLikeListCreateView, login_user, logout_user
+)
+from . import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='api-index'),  # Redirect to index.html
+    path('bands/', views.BandListView.as_view(), name='band-list'),
+    path('bands/create/', views.BandCreateView.as_view(), name='band-list-create'),
     path('bands/', BandListCreateView.as_view(), name='band-list-create'),
+    path('bands/<int:band_id>/', BandDetailView.as_view(), name='band-detail'),
+    path('bands/<int:band_id>/comments/', BandCommentListView.as_view(), name='band-comment-list-create'),
     path('albums/', AlbumListCreateView.as_view(), name='album-list-create'),
+    path('albums/<int:album_id>/', AlbumDetailView.as_view(), name='album-detail'),
+    path('albums/<int:album_id>/comments/', AlbumCommentListView.as_view(), name='album-comment-list-create'),
     path('songs/', SongListCreateView.as_view(), name='song-list-create'),
     path('reviews/', AlbumReviewListCreateView.as_view(), name='album-review-list-create'),
     path('likes/', AlbumReviewLikeListCreateView.as_view(), name='album-review-like-list-create'),
@@ -12,4 +25,9 @@ urlpatterns = [
     path('likes/<int:pk>/', LikeDeleteView.as_view(), name='like-delete'),
     path('songs/<int:song_id>/comments/', CommentList.as_view(), name='song-comment-list-create'),
     path('comments/', AllCommentList.as_view(), name='all-comment-list'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('songs/likes/', SongLikeListCreateView.as_view(), name='song-like-list-create'),
+    # Add other URL patterns for your API paths here
+    path('api-auth/', include('rest_framework.urls')),  # Include DRF authentication URLs
 ]
--- postit_api/views.py
+++ postit_api/views.py
@@ -2,22 +2,77 @@ from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from .models import Band, Album, Song, AlbumReview, AlbumReviewComment, AlbumReviewLike, Comment
 from .serializers import BandSerializer, AlbumSerializer, SongSerializer, AlbumReviewSerializer, AlbumReviewCommentSerializer, AlbumReviewLikeSerializer, CommentSerializer
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Comment
-from .serializers import CommentSerializer
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+
 
 
 class BandListCreateView(generics.ListCreateAPIView):
     queryset = Band.objects.all()
     serializer_class = BandSerializer
 
+
+class BandCreateView(APIView):
+    def post(self, request, format=None):
+        serializer = BandSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BandListView(ListView):
+    model = Band
+    serializer_class = BandSerializer
+    template_name = 'bands.html'
+    context_object_name = 'bands'
+
+    def get_queryset(self):
+        return Band.objects.all()
+
+class BandDetailView(generics.RetrieveAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+# Rename BandCommentListCreateView to BandCommentListView
+class BandCommentListView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        band_id = self.kwargs['band_id']
+        return Comment.objects.filter(band_id=band_id)
+
+    def perform_create(self, serializer):
+        band_id = self.kwargs['band_id']
+        serializer.save(user=self.request.user, band_id=band_id)
+
 class AlbumListCreateView(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
-class SongListCreateView(generics.ListCreateAPIView):
-    queryset = Song.objects.all()
-    serializer_class = SongSerializer
+class AlbumDetailView(generics.RetrieveAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+# Rename AlbumCommentListCreateView to AlbumCommentListView
+class AlbumCommentListView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        album_id = self.kwargs['album_id']
+        return Comment.objects.filter(album_id=album_id)
+
+    def perform_create(self, serializer):
+        album_id = self.kwargs['album_id']
+        serializer.save(user=self.request.user, album_id=album_id)
 
 class AlbumReviewListCreateView(generics.ListCreateAPIView):
     queryset = AlbumReview.objects.all()
@@ -34,7 +89,7 @@ class AlbumReviewLikeListCreateView(generics.ListCreateAPIView):
 class AlbumReviewDeleteView(generics.DestroyAPIView):
     queryset = AlbumReview.objects.all()
     serializer_class = AlbumReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -48,11 +103,10 @@ class SongDeleteView(generics.DestroyAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
 
-
 class LikeDeleteView(generics.DestroyAPIView):
     queryset = AlbumReviewLike.objects.all()
     serializer_class = AlbumReviewLikeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -61,7 +115,6 @@ class LikeDeleteView(generics.DestroyAPIView):
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response({'message': 'You do not have permission to delete this like.'}, status=status.HTTP_403_FORBIDDEN)
-
 
 class SongCommentListCreateView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
@@ -89,8 +142,50 @@ class CommentList(generics.ListCreateAPIView):
         song = Song.objects.get(pk=song_id)
         serializer.save(user=self.request.user, song=song)
 
-
 class AllCommentList(generics.ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+class SongListCreateView(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('song-list-create')  # Redirect to song list after successful login
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+
+class SongLikeListCreateView(generics.ListCreateAPIView):
+    queryset = AlbumReviewLike.objects.all()
+    serializer_class = AlbumReviewLikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        song_id = self.request.query_params.get('song_id', None)
+        if song_id is not None:
+            return AlbumReviewLike.objects.filter(song_id=song_id)
+        return AlbumReviewLike.objects.all()
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('Jūs jau palikote patiktuką šiai dainelei!')
+        song = Song.objects.get(pk=self.kwargs['song_id'])
+        serializer.save(user=self.request.user, song=song)
+
+
+from .views import logout_user, login_user, SongLikeListCreateView
